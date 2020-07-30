38d9fe83809e4cae9592d1a8623dd33979c08a4c
Gabriel Henriquez Alguacil <gabha2107@gmail.com>
2020-07-29 23:45:59 -0400

    Added a navigation bar with animations

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "./NavigationBar.css";
+
+class NavigationBar extends Component {
+  render() {
+    return (
+      <header className="navigation-bar">
+        <nav className="navigation-bar__navigation">
+          <div className="navigation-bar__logo">
+            <a href="/">Logo</a>
+          </div>
+          <div className="spacer"></div>
+          <div className="navigation-bar__list">
+            <ul>
+              <li>
+                <a href="/">
+                  <FontAwesomeIcon icon="home" size="1x" />
+                </a>
+              </li>
+              <li>
+                <a href="/">Services</a>
+              </li>
+              <li>
+                <a href="/">Products</a>
+              </li>
+              <li>
+                <a href="/">About Us</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default NavigationBar;
