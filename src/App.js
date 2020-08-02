67a76168e3838910d1f83a7975e57eee8fecc99a
Gabriel Henriquez Alguacil <gabha2107@gmail.com>
2020-08-01 23:21:36 -0400

    remove out the highlight card from app.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ import "./components/common_styles/FontAwesomeIcon";
 
 import IntroSlider from "./components/IntroSlider/IntroSlider";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-import Highlight from "./components/HighlightCard/HighlightCard";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
       <IntroSlider />
-      <Highlight
-        highlightCard="borderRight"
-        highlightHeader="hello"
-        highlightDescription="Fugiat sit culpa nulla ullamco cupidatat esse enim laboris enim minim nulla deserunt."
-      />
     </div>
   );
 }
