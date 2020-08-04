c038b0aaeaa8ac7f7d13a60ddeedc21400753998
Gabriel Henriquez Alguacil <gabha2107@gmail.com>
2020-08-04 16:10:24 -0400

    Footer Component created

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <Fragment>
+          <h1>Say something</h1>
+        </Fragment>
+        <Fragment>
+          <h1>in each one</h1>
+        </Fragment>
+        <Fragment>
+          <h1>or not</h1>
+        </Fragment>
+        <h4>
+          Powered by Gabriel H. Alguacil in Santo Domingo, Dominican Republic.
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
